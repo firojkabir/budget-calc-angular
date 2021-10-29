fd83b69d4bdab123b03c5eba5af186b6e9df04f0
Md Firoj Kabir <firoj.kabir65@gmail.com>
2021-10-29 20:21:10 +0200

    Create a modal to update card item

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BudgetItem } from 'src/shared/models/budget-item.model';
+import { MatDialog } from '@angular/material/dialog';
+import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.component';
 
 @Component({
   selector: 'app-budget-item-list',
@@ -8,14 +10,27 @@ import { BudgetItem } from 'src/shared/models/budget-item.model';
 })
 export class BudgetItemListComponent implements OnInit {
   @Input()
-  budgetItems: BudgetItem[] | undefined;
+  budgetItems!: BudgetItem[];
   @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
   onDeleteButtonClick(item: BudgetItem) {
     this.delete.emit(item);
+  }
+
+  onCardClicked(item: BudgetItem) {
+    const dialogRef = this.dialog.open(EditItemModalComponent, {
+      width: '580px',
+      data: item,
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result) {
+        this.budgetItems[this.budgetItems.indexOf(item)] = result;
+      }
+    });
   }
 }

b66d7258e9984559917efc24f3aa88f059b432e2
Md Firoj Kabir <firoj.kabir65@gmail.com>
2021-10-29 16:37:23 +0200

    Show categorized amount

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-add-item-form',
   templateUrl: './add-item-form.component.html',
-  styleUrls: ['./add-item-form.component.scss']
+  styleUrls: ['./add-item-form.component.scss'],
 })
 export class AddItemFormComponent implements OnInit {
+  @Input() item: BudgetItem = new BudgetItem('', null);
+  @Output() formSubmit: EventEmitter<BudgetItem> =
+    new EventEmitter<BudgetItem>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    this.formSubmit.emit(form.value);
   }
-
 }
